ca3d78df69c9db5cb2ea38c200bd61680015e11e
JonathanVanWin <grandearbol@gmail.com>
2019-06-24 17:17:32 +0300

    Initial Commit to ProjectEuler-Java

---
+++ src/Mult1ples3_5.java
@@ -0,0 +1,12 @@
+
+public class Mult1ples3_5 {
+
+	public static void main(String[] args) {
+		int n3=999/3;
+		int n5=995/5;
+		int sn3=(n3/2)*(3+999);
+		int sn5=(n5/2)*(5+995);
+		System.out.println(sn3+sn5);
+	}
+
+}
